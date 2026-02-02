6a7ef29d8afa9ede8b892135e2ed62a465dfe07a
Borworn Kultumyotin <177917303+Byffify@users.noreply.github.com>
2026-02-03 00:42:26 +0700

    Lab 4

---
+++ int250-B1-02-L4-67130500017-Borworn/my-profile-card/src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import ProfileCard from "./components/ProfileCard.vue";
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-center gap-8 min-h-screen bg-gradient-to-br from-red-900 to-sky-400"
+  >
+    <ProfileCard
+      background="./src/assets/backgroundpluem.png"
+      profileimg="./src/assets/profilepluem.png"
+      name="Korawit Rungthampaisan"
+      subheading="Student, 20, BKK"
+      followers="100M"
+      followings="10"
+      projects="20K"
+      facebook="Korawit Rungthampaisan"
+      instagram="px_krwit"
+      tiktok="polomoume"
+      facebookurl="https://www.facebook.com/borworn.kulthumyotin"
+      instagramurl="https://www.instagram.com/px_krwit/"
+      tiktokurl="https://www.tiktok.com/@polomoume?_r=1&_t=ZS-93aZAEQ5rhU"
+    />
+    <ProfileCard
+      background="./src/assets/background.jpg"
+      profileimg="./src/assets/profilefiat.jpg"
+      name="Borworn Kultumyotin"
+      subheading="Student, 20, BKK"
+      followers="620"
+      followings="580"
+      projects="20"
+      facebook="Borworn Kultumyotin"
+      instagram="fiatkul"
+      tiktok="Byffify"
+      facebookurl="https://www.facebook.com/borworn.kulthumyotin"
+      instagramurl="https://www.instagram.com/fiatkul/"
+      tiktokurl="https://www.tiktok.com/@byffify16?_r=1&_t=ZS-93aYX6Fiz0H"
+    /> 
+    <ProfileCard
+      background="./src/assets/backgroundguide.png"
+      profileimg="./src/assets/profileguide.jpg"
+      name="Peerapol Pholphaka"
+      subheading="Student, 20, BKK"
+      followers="100T"
+      followings="20"
+      projects="200M"
+      facebook="แดเนี่ยล ปลาสลิด"
+      instagram="daniel_plaslid"
+      tiktok="daplaslid"
+      facebookurl="https://www.facebook.com/d.nei.yl.pla.slid?rdid=GVCmlGYNOoKQBSAv&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1BLcggG1rG%2F#"
+      instagramurl="https://www.instagram.com/daniel_plaslid/"
+      tiktokurl="https://www.tiktok.com/@daplaslid?_r=1&_t=ZS-93aa10jbIaj"
+    />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ int250-B1-02-L4-67130500017-Borworn/my-profile-card/src/components/ProfileCard.vue
@@ -0,0 +1,98 @@
+<script setup>
+defineProps([
+  "profileimg",
+  "background",
+  "name",
+  "subheading",
+  "followers",
+  "followings",
+  "projects",
+  "facebook",
+  "instagram",
+  "tiktok",
+  "facebookurl",
+  "instagramurl",
+  "tiktokurl",
+]);
+</script>
+
+<template>
+  <!-- Card -->
+  <div
+    class="bg-white shadow-lg rounded-2xl min-w-[320px] overflow-hidden text-center"
+  >
+    <!-- Image Header / Profile Picture -->
+    <div
+      class="relative min-h-32 bg-cover bg-center"
+      :style="'background-image: url(' + background + ')'"
+    >
+      <img
+        :src="profileimg"
+        alt="Profile Image"
+        class="w-32 h-32 rounded-full object-cover border-4 border-white absolute top-full left-1/2 -translate-x-1/2 -translate-y-1/2"
+      />
+    </div>
+
+    <div class="px-6 pt-20 pb-6">
+      <h2 class="font-bold text-gray-800 text-2xl mb-2">{{ name }}</h2>
+      <h3 class="text-gray-600 text-lg">{{ subheading }}</h3>
+    </div>
+
+    <!-- Social Links -->
+    <div class="border-t border-gray-200 p-6">
+      <div class="flex justify-center gap-8">
+        <a
+          :href="facebookurl"
+          target="_blank"
+          class="flex items-center gap-2 text-gray-700 hover:text-blue-600 transition"
+        >
+          <i class="fa-brands fa-facebook text-xl"></i>
+          <span class="font-semibold">{{ facebook }}</span>
+        </a>
+
+        <a
+          :href="instagramurl"
+          target="_blank"
+          class="flex items-center gap-2 text-gray-700 hover:text-pink-300 transition"
+        >
+          <i class="fa-brands fa-instagram text-xl"></i>
+          <span class="font-semibold">{{ instagram }}</span>
+        </a>
+
+        <a
+          :href="tiktokurl"
+          target="_blank"
+          class="flex items-center gap-2 text-gray-700 hover:text-black transition"
+        >
+          <i class="fa-brands fa-tiktok text-xl"></i>
+          <span class="font-semibold">{{ tiktok }}</span>
+        </a>
+      </div>
+    </div>
+
+    <!-- Stats -->
+    <div
+      class="p-6 border-t border-gray-200 flex item-center justify-center gap-6"
+    >
+      <!-- Follwers -->
+      <div>
+        <h4 class="text-gray-600 text-sm">Followers</h4>
+        <p class="font-bold text-gray-800 text-2xl">{{ followers }}</p>
+      </div>
+
+      <!-- Followings -->
+      <div>
+        <h4 class="text-gray-600 text-sm">Following</h4>
+        <p class="font-bold text-gray-800 text-2xl">{{ followings }}</p>
+      </div>
+
+      <!-- Project -->
+      <div>
+        <h4 class="text-gray-600 text-sm">Project</h4>
+        <p class="font-bold text-gray-800 text-2xl">{{ projects }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
