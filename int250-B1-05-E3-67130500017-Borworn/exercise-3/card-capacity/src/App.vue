806eaa735e3c5d8dc5736a331b9c6fbfc5cf4640
Borworn Kultumyotin <177917303+Byffify@users.noreply.github.com>
2026-02-24 13:08:06 +0700

    Class05-E3-Exercise-3

---
+++ int250-B1-05-E3-67130500017-Borworn/exercise-3/card-capacity/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Card from "./components/Card.vue";
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-center gap-8 min-h-screen w-full bg-indigo-950"
+  >
+    <Card level="BASIC" capacity="100GB" price="$1.99" subcapacity="100 GB" />
+    <Card
+      level="STANDARD"
+      capacity="200GB"
+      price="$3.99"
+      subcapacity="200 GB"
+    />
+    <Card level="PREMIUM" capacity="2TB" price="$8.99" subcapacity="2 TB" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ int250-B1-05-E3-67130500017-Borworn/exercise-3/card-capacity/src/components/Card.vue
@@ -0,0 +1,53 @@
+<script setup>
+defineProps(["level", "capacity", "price", "subcapacity"]);
+</script>
+
+<template>
+  <!-- Card section -->
+  <section>
+    <!-- Outline Container -->
+    <div
+      class="group w-72 border-11 border-gray-700 hover:border-violet-500 rounded-2xl p-3 transition"
+    >
+    <!-- Inline Container -->
+      <div class="rounded-xl bg-indigo-950">
+        <!-- Card content -->
+        <div class="flex flex-col items-center text-white py-5">
+          <!-- Plan Level -->
+          <p>{{ level }}</p>
+          <div class="p-5 flex flex-col items-center gap-5">
+            <!-- Capacity Info -->
+            <div class="flex flex-col items-center">
+              <!-- Capacity -->
+              <h1 class="capacity">{{ capacity }}</h1>
+              <!-- Price -->
+              <p>{{ price }}/month</p>
+            </div>
+            <!-- Purchase Button -->
+            <button
+              class="mb-6 px-10 py-3 border-violet-500 border-1 group-hover:bg-violet-500 rounded-lg"
+            >
+              Purchase
+            </button>
+          </div>
+        </div>
+        <!-- Card Feature -->
+        <div
+          class="flex flex-col items-center gap-3 py-8 text-white border-t-3 border-gray-700"
+        >
+        <!-- List Feature -->
+          <p><i class="fa-solid fa-check"></i>{{ subcapacity }}</p>
+          <p><i class="fa-solid fa-check"></i>Option to add members</p>
+          <p><i class="fa-solid fa-check"></i>Extra member benefits</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.capacity {
+  font-family: Georgia, serif;
+  font-size: 48px;
+}
+</style>
